6f6871fe45e4fc57ed04e84d2ba26bb7949acefc
ingogutwin <user.name>
2023-01-11 14:55:31 +0100

    New dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
-<script setup></script>
+<template>
+  <main>
+    <ToDos />
+  </main>
+</template>
 
-<template></template>
+<script setup>
+import ToDos from "./components/ToDos.vue";
+</script>
 
 <style scoped></style>
---
+++ src/components/ToDos.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <CheckButton />
+  </div>
+</template>
+
+<script setup>
+import CheckButton from "./icons/CheckButton.vue";
+</script>
+
+<style lang="scss" scoped></style>
