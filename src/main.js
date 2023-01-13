e348a9c504b0109767ead0d5c340e2f0cc6ae313
ingogutwin <user.name>
2023-01-13 13:19:44 +0100

    Make components global

--- src/main.js
+++ src/main.js
@@ -2,4 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/main.css";
 
-createApp(App).mount("#app");
+import AppWrapper from "./components/AppWrapper.vue";
+import ToDoItem from "./components/ToDoItem.vue";
+
+const app = createApp(App);
+
+app.component("AppWrapper", AppWrapper);
+app.component("ToDoItem", ToDoItem);
+
+app.mount("#app");
