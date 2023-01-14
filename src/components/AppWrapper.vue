e3961fcd8669d46de3ce6611849e6b7c6b165073
ingogutwin <user.name>
2023-01-14 07:44:57 +0100

    Add comments and format the code

--- src/components/AppWrapper.vue
+++ src/components/AppWrapper.vue
@@ -1,15 +1,19 @@
 <template>
+  <!-- start main container -->
   <div class="content">
-    <div
-      class="heading max-w-xl mx-auto flex justify-between items-center pt-20"
-    >
+    <!-- start container with heading and theme toggle -->
+    <div class="heading">
       <h1 class="text-5xl font-bold uppercase text-white pt-3">todo</h1>
       <button @click="$emit('changeTheme')" class="w-6 h-6 text-white">
         <IconSun v-if="currentTheme === 'dark'" />
         <IconMoon v-else-if="currentTheme === 'light'" />
       </button>
     </div>
+    <!-- end container  -->
+    <!-- start container todo's input -->
+    <!-- end container todo's input -->
   </div>
+  <!-- end main container -->
 </template>
 
 <script>
@@ -23,5 +27,8 @@ export default {
 <style scoped>
 .content {
   @apply bg-light_theme-very-light-gray dark:bg-dark_theme-very-dark-blue h-screen bg-no-repeat bg-bg-desktop-light dark:bg-bg-desktop-dark bg-top w-full;
+}
+.heading {
+  @apply max-w-xl mx-auto flex justify-between items-center pt-20;
 }
 </style>
