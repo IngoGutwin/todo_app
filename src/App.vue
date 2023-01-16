fd40e8d462fc56accf292d0c3dfdeb1ede3cafcd
ingogutwin <user.name>
2023-01-16 15:02:11 +0100

    Update theme swich

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,28 @@
 export default {
   data() {
     return {
-      currentTheme: "light",
+      currentTheme: "",
     };
   },
+  created() {
+    this.checkForSavedTheme();
+  },
   methods: {
+    checkForSavedTheme() {
+      const savedTheme = localStorage.getItem("theme");
+      if (savedTheme) {
+        this.currentTheme = savedTheme;
+      } else {
+        this.changeTheme();
+      }
+    },
     changeTheme() {
       if (this.currentTheme === "light") {
         this.currentTheme = "dark";
+        localStorage.setItem("theme", this.currentTheme);
       } else {
         this.currentTheme = "light";
+        localStorage.setItem("theme", this.currentTheme);
       }
     },
   },
