72403b8b1a5eb691ed639f5ca435f35134213a19
ingogutwin <user.name>
2023-01-17 15:02:36 +0100

    Make imports

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ import ToDoItem from "./components/ToDoItem.vue";
 import IconMoon from "./components/icons/IconMoon.vue";
 import IconSun from "./components/icons/IconSun.vue";
 import CrossIcon from "./components/icons/CrossIcon.vue";
+import CheckIcon from "./components/icons/CheckIcon.vue";
+import ToDoListWrapper from "./components/ToDoListWrapper.vue";
+import UserInput from "./components/UserInput.vue";
 
 const app = createApp(App);
 
@@ -15,5 +18,8 @@ app.component("ToDoItem", ToDoItem);
 app.component("IconMoon", IconMoon);
 app.component("IconSun", IconSun);
 app.component("CrossIcon", CrossIcon);
+app.component("CheckIcon", CheckIcon);
+app.component("ToDoListWrapper", ToDoListWrapper);
+app.component("UserInput", UserInput);
 
 app.mount("#app");
