47ad66020c2c7e48fea028d56eef98377f40c077
ingogutwin <user.name>
2023-01-17 15:02:59 +0100

    Make ToDoItem

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="mx-auto max-w-xl">
-    <p>a new todo</p>
+  <div
+    class="mx-auto max-w-xl todo-item flex justify-between self-center items-center border-b-2 border-light_theme-light-grayish-blue dark:border-dark_theme-dark-grayish-blue"
+  >
+    <button>
+      <slot name="check"></slot>
+    </button>
+
+    <div class="w-full px-2">
+      <slot></slot>
+    </div>
+
+    <button>
+      <slot name="cross"></slot>
+    </button>
   </div>
 </template>
-
-<script setup></script>
 
 <style></style>
