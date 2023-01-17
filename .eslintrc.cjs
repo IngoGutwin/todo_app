76a24bc7afba5908364f0e6903be6f730a2aff19
ingogutwin <user.name>
2023-01-17 15:02:29 +0100

    Update code formater

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,12 @@
 /* eslint-env node */
+
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
+  env: {
+    node: true,
+  },
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
@@ -10,5 +14,11 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: "latest",
+  },
+  rules: {
+    "vue/padding-line-between-tags": [
+      "error",
+      [{ blankLine: "always", prev: "*", next: "*" }],
+    ],
   },
 };
